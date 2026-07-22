07f4bf7ae321e221baa895c8fcb01ae7808ee8c1
Yael123R <yaelyusefhm123@gmail.com>
2026-07-21 20:42:12 -0500

    feat: Historias de Usuario 1 Checkpoint.

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const pokemonLocal = [
+  {
+    nombre: "bulbasaur",
+    imagen:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    tipos: ["grass", "poison"],
+  },
+  {
+    nombre: "charmander",
+    imagen:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    tipos: ["fire"],
+  },
+  {
+    nombre: "squirtle",
+    imagen:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+    tipos: ["water"],
+  },
+  {
+    nombre: "pikachu",
+    imagen:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    tipos: ["electric"],
+  },
+  {
+    nombre: "jigglypuff",
+    imagen:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png",
+    tipos: ["normal", "fairy"],
+  },
+  {
+    nombre: "gengar",
+    imagen:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png",
+    tipos: ["ghost", "poison"],
+  },
+];
